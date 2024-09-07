aa475a015285ef488f103eb016d764fdec53cd05
Justin Zhang <schnell18@gmail.com>
2024-09-08 11:44:38 +1200

    Simplify dependencies, add tensor functions

--- src/lm_quant_toolkit/utils/safetensors.py
+++ src/lm_quant_toolkit/utils/safetensors.py
@@ -17,6 +17,23 @@ def get_tensor(matrix_name, base_dir, index_json="model.safetensors.index.json")
             raise ValueError(f"Invalid key {matrix_name}")
 
 
+def get_tensor2(matrix_name, st_file_fp):
+    try:
+        with safe_open(st_file_fp, framework="pt", device="cpu") as f:
+            return f.get_tensor(matrix_name)
+    except Exception:
+        raise ValueError("Fail to retrieve tensor keys")
+
+
+def get_tensor_keys(base_dir, st_file):
+    try:
+        mp = os.path.join(base_dir, st_file)
+        with safe_open(mp, framework="pt", device="cpu") as f:
+            return f.keys()
+    except Exception:
+        raise ValueError("Fail to retrieve tensor keys")
+
+
 def get_tensor_dual(matrix1, matrix2, base_dir, st_file):
     try:
         mp = os.path.join(base_dir, st_file)
