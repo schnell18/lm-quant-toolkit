924a761385f134f1211c50f36c3fb652df418278
Justin Zhang <schnell18@gmail.com>
2026-04-03 16:48:21 +1300

    Switch to vu
    Signed-off-by: Justin Zhang <schnell18@gmail.com>

--- setup-harness.sh
+++ setup-harness.sh
@@ -7,46 +7,43 @@ if [[ $? -ne 0 ]]; then
     exit 1
 fi
 
-# check if miniconda is installed
-if [[ ! -d ~/miniconda3 ]]; then
-    echo "miniconda is not installed!"
-    exit 2
-fi
-
-# check the current activated env
-if [[ $CONDA_DEFAULT_ENV == "base" ]]; then
-    echo "Please switch to an environment other than base!"
-    exit 3
-fi
-
 OLD_DIR=$(pwd)
-
-# perform an editable install of the lm-quant-toolkit project
-# to install all dependencies that are published on PyPI
-pip install -e .
 
 # install the patched dependencies from source
 # clone patched AutoGPTQ from https://github.com/schnell18/AutoGPTQ.git
-git clone https://github.com/schnell18/AutoGPTQ.git .deps/AutoGPTQ
-cd .deps/AutoGPTQ
-pip install -e .
-cd $OLD_DIR
+# git clone https://github.com/schnell18/AutoGPTQ.git .deps/AutoGPTQ
+# cd .deps/AutoGPTQ
+# uv pip install -e .
+# cd $OLD_DIR
 
 
 # clone patched hqq from https://github.com/schnell18/hqq.git
-git clone https://github.com/schnell18/hqq.git .deps/hqq
+if [ ! -d .deps/hqq ]; then
+    git clone https://github.com/schnell18/hqq.git .deps/hqq
+fi
 cd .deps/hqq
-pip install -e .
+uv pip install -e .
 cd $OLD_DIR
 
 # clone patched lm-evaluation-harness from https://github.com/schnell18/lm-evaluation-harness.git
-git clone https://github.com/schnell18/lm-evaluation-harness.git .deps/lm-evaluation-harness
+if [ ! -d .deps/lm-evaluation-harness ]; then
+    git clone https://github.com/schnell18/lm-evaluation-harness.git .deps/lm-evaluation-harness
+fi
 cd .deps/lm-evaluation-harness
-pip install -e .
+uv pip install -e .
 cd $OLD_DIR
 
 # clone patched lm-evaluation-harness from https://github.com/schnell18/CLIP_benchmark.git
-git clone https://github.com/schnell18/CLIP_benchmark.git .deps/CLIP_benchmark
+if [ ! -d .deps/CLIP_benchmark ]; then
+    git clone https://github.com/schnell18/CLIP_benchmark.git .deps/CLIP_benchmark
+fi
 cd .deps/CLIP_benchmark
-pip install -e .
+git pull
+uv pip install -e .
 cd $OLD_DIR
+
+
+# perform an editable install of the lm-quant-toolkit project
+# to install all dependencies that are published on PyPI
+uv pip install -e .
+
