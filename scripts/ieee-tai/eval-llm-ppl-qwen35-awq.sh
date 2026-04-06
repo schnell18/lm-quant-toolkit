25c1c77eabc445fa7a860ae41ca65d96f08606b7
Justin Zhang <schnell18@gmail.com>
2026-04-07 08:29:40 +1200

    Add baseline measuring scripts and results
    Signed-off-by: Justin Zhang <schnell18@gmail.com>

---
+++ scripts/ieee-tai/eval-llm-ppl-qwen35-awq.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# export HF_HOME=/data/hugginface
+# conda activate quant-eval
+
+EXP_RESULT_BASE_DIR=/fdata/llm/ieee-tai
+if [ ! -d $EXP_RESULT_BASE_DIR/logs ]; then
+    mkdir $EXP_RESULT_BASE_DIR/logs
+fi
+
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+
+python ../../src/cli.py llm \
+    --task eval_ppl \
+    --model Qwen/Qwen3.5-2B Qwen/Qwen3.5-4B Qwen/Qwen3.5-9B \
+    --algo awq \
+    --config b4g32 b4g64 b4g128 \
+    --experiment-name eval_ppl-qwen35-awq \
+    --quant-snapshot-dir="$EXP_RESULT_BASE_DIR/snapshots" \
+    --result-dir="$EXP_RESULT_BASE_DIR/results" \
+    2>&1 \
+    | tee "$EXP_RESULT_BASE_DIR/logs/bench-$(date +%Y%m%d%H%M%S).log"
