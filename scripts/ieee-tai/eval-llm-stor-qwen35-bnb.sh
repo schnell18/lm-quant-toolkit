76f7fe27c69a42c481cdc0e7f16d444d84661aa1
Justin Zhang <schnell18@gmail.com>
2026-04-12 21:24:04 +1200

    Fix bnb storage data
    Signed-off-by: Justin Zhang <schnell18@gmail.com>

--- scripts/ieee-tai/eval-llm-stor-qwen35-bnb.sh
+++ scripts/ieee-tai/eval-llm-stor-qwen35-bnb.sh
@@ -2,26 +2,28 @@
 
 # export HF_HOME=/data/hugginface
 
-if [ ! -d logs ]; then
-    mkdir logs
+EXP_RESULT_BASE_DIR=/fdata/llm/ieee-tai
+if [ ! -d $EXP_RESULT_BASE_DIR/logs ]; then
+    mkdir $EXP_RESULT_BASE_DIR/logs
 fi
+
 
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 algo=bnb
-model_ids="0 1 2"
+model_ids="Qwen/Qwen3.5-2B Qwen/Qwen3.5-4B Qwen/Qwen3.5-9B"
 cfgs="b4g64 b8g128"
 for m in $model_ids; do
     for cfg in $cfgs ; do
         python ../../src/cli.py llm \
-            --quant-snapshot-dir="/fdata/llm/ieee-tai/snapshots2" \
-            --result-dir="/fdata/llm/ieee-tai/results" \
+            --quant-snapshot-dir="$EXP_RESULT_BASE_DIR/snapshots2" \
+            --result-dir="$EXP_RESULT_BASE_DIR/results" \
             --model $m \
             --algo ${algo} \
             --config ${cfg} \
             --task eval_model_storage \
-            --experiment-name eval_model_storge-bnb \
+            --experiment-name eval_model_stor-bnb \
             2>&1 \
-            | tee logs/bench-$(date +%Y%m%d%H%M%S).log
+            | tee "$EXP_RESULT_BASE_DIR/logs/bench-$(date +%Y%m%d%H%M%S).log"
     done
 done
